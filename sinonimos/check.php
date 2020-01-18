f535107a3b856fbd89ee035c56dfa22d689c3780
Jhoseph <jhoseph.dev@gmail.com>
2020-01-18 02:07:12 +0100

    Analiza todas las faq para detectar todos los sinonimos existentes

--- sinonimos/check.php
+++ sinonimos/check.php
@@ -3,18 +3,14 @@ require_once __DIR__ .'/../funciones.php';
 require_once __DIR__ .'/../FaqSinonimos.php';
 $link = conectar();
 $faq_id = 1;
-$sql = "SELECT * FROM portales.faqs WHERE id = $faq_id";
+$sql = "SELECT * FROM portales.faqs";
 $rs = mysqli_query($link,$sql);
 while ($row = mysqli_fetch_assoc($rs)){
     $sinonimos_id = analizaFaq($link,$row['respuesta']);
-    $faqs_sinonimos_id = trim($row['faq_sinonimos_id'],',');
-    $faqs_sinonimos_id = explode(',',$faqs_sinonimos_id);
-    $faqs_sinonimos_id = array_merge($faqs_sinonimos_id,$sinonimos_id);
-    $faqs_sinonimos_id = array_unique($faqs_sinonimos_id);
-    $faqs_sinonimos_id = implode(',',$faqs_sinonimos_id);
-    if ($faqs_sinonimos_id != $row['faqs_sinonimos_id']){
-        $sql = "UPDATE portales.faqs SET faq_sinonimos_id = '$faqs_sinonimos_id' WHERE id = $row[id]";
-        $rs = mysqli_query($link,$sql);
+    $sinonimos_id = implode(',',$sinonimos_id);
+    if ($sinonimos_id != $row['faqs_sinonimos_id']){
+        $sql = "UPDATE portales.faqs SET faq_sinonimos_id = '$sinonimos_id' WHERE id = $row[id]";
+        $rs2 = mysqli_query($link,$sql);
     }
 }
 
